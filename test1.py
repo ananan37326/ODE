9e0ec4c8560f7d1ddb7f4f90b81aa184bee77470
Tousif Anan <1705053@ugrad.cse.buet.ac.bd>
2019-08-29 02:31:45 +0600

    new

---
+++ test1.py
@@ -0,0 +1,45 @@
+import numpy as np
+from math import sin
+import matplotlib.pyplot as plt
+
+def func(x,y):
+    return (x+20*y) * sin(x*y)
+
+def Euler(x,h):
+    n = len(x)
+    y = np.zeros(n)
+    y[0] = 4
+    for i in range(0,n-1):
+        y[i+1] = y[i] + func(x[i],y[i]) * h
+    return y
+
+step = 0.01
+x1 = np.arange(0,10+step,step=step)
+x1 = np.around(x1,decimals=2)
+
+y1 = Euler(x1,step)
+
+step = 0.05
+x2 = np.arange(0,10+step,step=step)
+x2 = np.around(x2,decimals=2)
+y2 = Euler(x2,step)
+
+step = 0.1
+x3 = np.arange(0,10+step,step=step)
+x3 = np.around(x3,decimals=2)
+y3 = Euler(x3,step)
+
+step = 0.5
+x4 = np.arange(0,10+step,step=step)
+x4 = np.around(x4,decimals=2)
+y4 = Euler(x4,step)
+
+
+plt.plot(x1,y1,linestyle='-',marker='.',color='red',label='Step Size 0.01')
+plt.plot(x2,y2,linestyle='-',marker='.',color='blue',label='Step Size 0.05')
+plt.plot(x3,y3,linestyle='-',marker='.',color='green',label='Step Size 0.1')
+plt.plot(x4,y4,linestyle='-',marker='.',color='yellow',label='Step Size 0.5')
+plt.grid()
+plt.legend()
+plt.show()
+
